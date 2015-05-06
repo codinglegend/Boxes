e9cae1d1112f9ac24ed403665a4e605ab8bcc46b
Alain K <alain.kahwaji@gmail.com>
2015-05-05 18:01:42 -0700

    fix boxes

--- Boxes/main.c
+++ Boxes/main.c
@@ -8,8 +8,48 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+typedef struct{
+    float h;
+    float l;
+    float w;
+} Box;
+
+float calculateVolume(Box box){
+    return box.h * box.l * box.w ;
 }
+
+Box makeBox(float height, float length, float width){
+    
+    Box b;
+    b.h = height;
+    b.l = length;
+    b.w = width;
+    
+    return b;
+}
+
+float calculateTimesVolume(Box box1, Box box2){
+    return (calculateVolume(box1)/calculateVolume(box2));
+}
+
+int main()
+{
+
+    Box b;
+    b.h = 5;
+    b.l = 5;
+    b.w = 5;
+
+    
+    Box c = makeBox(10, 10, 10);
+    
+    float something = calculateVolume(b);
+    printf("%f\n", something);
+    
+    float timesbigger = calculateTimesVolume(c, b);
+    printf("%f\n", timesbigger);
+   
+    
+}
+
+
